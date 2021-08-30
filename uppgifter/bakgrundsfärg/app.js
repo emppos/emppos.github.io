f2c5d41774d0d9c0c929a2da496c82c832a46010
emppos <71625358+emppos@users.noreply.github.com>
2021-08-30 13:54:18 +0300

    jj

---
+++ uppgifter/bakgrundsfärg/app.js
@@ -0,0 +1,25 @@
+const redInput = document.getElementById('red');
+const greenInput = document.getElementById('green');
+const blueInput = document.getElementById('blue');
+
+redInput.value = 255;
+greenInput.value = 255;
+blueInput.value = 255;
+
+redInput.addEventListener('input', onInput);
+greenInput.addEventListener('input', onInput);
+blueInput.addEventListener('input', onInput);
+
+function onInput(e) {
+    
+    const num = parseInt(e.target.value);
+
+    if(isNaN(num)){
+        e.target.value = 0;
+    }
+    else{
+        e.target.value = num;
+    }
+    
+    document.body.style.background = `rgb(${redInput.value}, ${greenInput.value}, ${blueInput.value})`;
+}
